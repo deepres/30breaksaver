34079abdef0657d833a4eacec1e09ce2521de13e
Piotr Dziubecki <piotr.dziubecki@panalpina.com>
2019-08-05 16:18:02 +0200

    updated code for multiple entries handling

--- popup.js
+++ popup.js
@@ -1,3 +1,5 @@
+var badgins = [];
+
 document.addEventListener('DOMContentLoaded', function () {
 
 	var checkPageButton = document.getElementById('checkPage');
@@ -7,27 +9,30 @@ document.addEventListener('DOMContentLoaded', function () {
 	}, function (tabs) {
 
 		chrome.tabs.executeScript({
-			code: 'document.querySelector("iframe").contentWindow.document.body.querySelector("#buch0").innerHTML'
+			code: 'document.querySelector("iframe").contentWindow.document.querySelector(\'body\').innerHTML'
 		}, display_h1);
 	});
 }, false);
 
 function display_h1(results) {
 
+
 	if (results == null || results == '') {
 		return;
 	}
 
-	var start_time = '' + results;
-	var hh, mm;
+	var tempDiv = document.createElement("DIV");
+	tempDiv.innerHTML = results;
+	var divs = tempDiv.querySelectorAll("div[id^='buch']")
+	
+	divs.forEach(convert_badgin);
+
+	var fbadgins = badgins.sort(function(a, b){return a-b}).filter(function(a){ return a > 0;})
+
 	var eight_min = 8 * 60 + 40 + 30;
 	var eight_max = 8 * 60 + 59 + 30;
 
-
-	hh = start_time.substring(0, 2);
-	mm = start_time.substring(3, 5);
-
-	var start_minutes = parseInt(hh) * 60 + parseInt(mm);
+	var start_minutes = fbadgins[0];
 
 	var end_min_minutes = start_minutes + eight_min;
 	var end_max_minutes = start_minutes + eight_max;
@@ -43,4 +48,29 @@ function display_h1(results) {
 	f.innerHTML += "<br>Latest dep time = " + displ_max_time;
 
 	d.body.appendChild(f);
+}
+
+function convert_badgin(element) {
+
+	var result = 0;
+	if (element.innerHTML.trim() == "") {
+
+		result = -1;
+
+	} else {
+
+		var start_time = '' + element.innerHTML;
+		var hh, mm;
+		var eight_min = 8 * 60 + 40 + 30;
+		var eight_max = 8 * 60 + 59 + 30;
+	
+	
+		hh = start_time.substring(0, 2);
+		mm = start_time.substring(3, 5);
+	
+		result = parseInt(hh) * 60 + parseInt(mm);
+	}
+
+	badgins.push(result);
+
 }
