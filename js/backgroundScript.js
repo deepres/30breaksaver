049bec9019c4c953db20f18c8abd97d47647c2b4
Piotr Dziubecki <piotr.dziubecki@panalpina.com>
2019-08-06 10:14:53 +0200

    changed to page action

--- js/backgroundScript.js
+++ js/backgroundScript.js
@@ -1,35 +1,19 @@
-chrome.tabs.onActivated.addListener(function(info){
-chrome.tabs.get(info.tabId, function(change){
-        if(change.url == undefined){
-            chrome.browserAction.setBadgeText( { text: '' } );
-            chrome.browserAction.setBadgeBackgroundColor({color: [0,0,0,0]});
-            console.log('undefined');
+// When the extension is installed or upgraded ...
+chrome.runtime.onInstalled.addListener(function() {
+    // Replace all rules ...
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+      // With a new rule ...
+      chrome.declarativeContent.onPageChanged.addRules([
+        {
+          // That fires when a page's URL contains a 'g' ...
+          conditions: [
+            new chrome.declarativeContent.PageStateMatcher({
+              pageUrl: { urlContains: 'ttsaas' }
+            })
+          ],
+          // And shows the extension's page action.
+          actions: [ new chrome.declarativeContent.ShowPageAction() ]
         }
-        else if(change.url.indexOf('ttsaas') < 0){
-            chrome.browserAction.setBadgeText( { text: '' } );
-            chrome.browserAction.setBadgeBackgroundColor({color: [0,0,0,0]});
-            console.log('not matching1');
-        }
-        else{
-            chrome.browserAction.setBadgeText( { text: '  ' } );
-            chrome.browserAction.setBadgeBackgroundColor({color: [50,200,50,100]});
-            console.log('matched');
-        }
+      ]);
     });
-});
-
-chrome.tabs.onUpdated.addListener(function (tabId, change, tab){
-    if(tab.url == undefined){
-        return;
-    }
-    else if(change.url.indexOf('ttsaas') < 0){
-        chrome.browserAction.setBadgeText( { text: '' } );
-        chrome.browserAction.setBadgeBackgroundColor({color: [0,0,0,0]});
-        console.log('not matching2');
-    }
-    else{
-        chrome.browserAction.setBadgeText( { text: '  ' } );
-        chrome.browserAction.setBadgeBackgroundColor({color: [50,200,50,100]});
-        console.log('matched');
-    }
-});
+  });
